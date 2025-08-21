c15ff76b81a77ea4cd680f0f3fa237cb3f5133c1
Mia <miaaaa0a@proton.me>
2025-08-21 12:31:44 +0300

    feat(web): copy upload url to clipboard

--- web/static/index.js
+++ web/static/index.js
@@ -1,6 +1,44 @@
 let submit = document.getElementById("submit");
 let content = document.getElementById("input");
 
+// https://stackoverflow.com/a/30810322
+function fallbackCopyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+  textArea.value = text;
+  
+  // Avoid scrolling to bottom
+  textArea.style.top = "0";
+  textArea.style.left = "0";
+  textArea.style.position = "fixed";
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+  var successful = false;
+
+  try {
+    var successful = document.execCommand('copy');
+  } catch (err) {
+    console.error('Fallback: Oops, unable to copy', err);
+  }
+
+  document.body.removeChild(textArea);
+  return successful;
+}
+
+function copyTextToClipboard(text) {
+  if (!navigator.clipboard) {
+    let success = fallbackCopyTextToClipboard(text);
+    return success;
+  }
+  navigator.clipboard.writeText(text).then(function() {
+    return true;
+  }, function(err) {
+    console.error('Async: Could not copy text: ', err);
+    return false;
+  });
+}
+
 submit.addEventListener("click", function (e) {
     let request = {
         code: 200,
@@ -18,7 +56,11 @@ submit.addEventListener("click", function (e) {
         let response = await r.json();
         console.log(response);
         if (response.code == 200) {
-            alert(`uploaded to\n    ${window.location.origin}/${response.content}`);
+            let upload_url = `${window.location.origin}/${response.content}`;
+            let success = copyTextToClipboard(upload_url);
+            let clipboard_info = success ? 'was not' : 'was';
+            alert(`uploaded to\n    ${upload_url}\n( url ${clipboard_info} copied to your clipboard ^_^ )`);
+            
         } else {
             alert(response.content);
         }
